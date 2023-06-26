188e3a1e923b18643fa6299a2813590262e6ce09
Brayand <nonalayavalderramabryan@gmail.com>
2023-06-26 00:02:53 -0500

    LISTAR PRODUCTOS LISTO

--- src/app/modules/producto/producto/producto.component.ts
+++ src/app/modules/producto/producto/producto.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ProductoService } from '../../shared/services/producto.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-producto',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productoService: ProductoService, public dialog: MatDialog,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    this.getProductos();
+  }
+
+  displayedColumns: string[] = ['idp', 'nombrep', 'stock', 'almacen', 'categoria','actions'];
+  dataSource = new MatTableDataSource<ProductoElement>();
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  getProductos(){
+    this.productoService.getProductos()
+      .subscribe((data:any) => {
+          console.log("respuesta de productos:", data);
+          this.processProductosResponse(data);
+      }, (error:any) => {
+        console.log("error en productos:", error);
+      })
+  }
+
+  processProductosResponse(resp:any){
+    const dataProductos: ProductoElement[] = [];
+
+    if(resp.metadata[0].code == "00"){
+
+      let listProducto = resp.productoResponse.productos;
+
+      listProducto.forEach((element: ProductoElement) => {
+        dataProductos.push(element);
+      });
+  
+      this.dataSource = new MatTableDataSource<ProductoElement>(dataProductos);
+      this.dataSource.paginator = this.paginator;
+    }   
   }
 
 }
+
+export interface ProductoElement{
+  idp: number;
+  nombrep: string;
+  stock: number;
+  almacen: any;
+  categoria: any;
+}
+
---
+++ src/app/modules/shared/services/producto.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const base_url = environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductoService {
+
+  constructor(private http: HttpClient) { }
+
+  getProductos(){
+    const endpoint = `${base_url}/productos`;
+    return this.http.get(endpoint);
+  }
+
+}
